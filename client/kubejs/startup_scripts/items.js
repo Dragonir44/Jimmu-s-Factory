281d0f872d639e7cd4ea6fd2eb982733c6c66dce
Dragonir44 <dragonir44@gmail.com>
2023-11-04 23:13:26 +0100

    change stack methode

--- client/kubejs/startup_scripts/items.js
+++ client/kubejs/startup_scripts/items.js
@@ -2,6 +2,7 @@ StartupEvents.registry('item', e => {
     e.create('elysium_sword', 'sword')
         .displayName('Elysium Sword')
         .tooltip('A sword made of Elysium')
-        .maxStackSize(1)
+        .unstackable()
         .tier('elysium_tier')
+        
 })

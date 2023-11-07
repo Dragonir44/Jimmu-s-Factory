e3a066833b8222195054476c6d0bb7b2c9d33bd5
Dragonir44 <dragonir44@gmail.com>
2023-11-07 19:35:10 +0100

    update

--- server/kubejs/startup_scripts/items.js
+++ server/kubejs/startup_scripts/items.js
@@ -1,8 +1,8 @@
 StartupEvents.registry('item', e => {
-    e.create('elysium_sword', 'sword')
+    e.create('elysium_sword_op', 'sword')
         .displayName('Elysium Sword')
         .tooltip('A sword made of Elysium')
         .unstackable()
         .tier('elysium_tier')
-        
+        .texture('kubejs:item/elysium_sword')
 })

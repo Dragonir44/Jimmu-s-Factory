1767c1a3e0db97334ae58e367a954a794159f496
Dragonir44 <dragonir44@gmail.com>
2023-11-11 22:45:16 +0100

    Add sunscreen beacon recipe and increase loot table chance.

--- server/kubejs/server_scripts/lootTable.js
+++ server/kubejs/server_scripts/lootTable.js
@@ -1,6 +1,6 @@
 LootJS.modifiers((event) => {
     event
         .addLootTableModifier(/.*chest.*/)
-        .randomChance(0.001)
+        .randomChance(0.01)
         .addLoot('kubejs:elysium_sword_op')
 })

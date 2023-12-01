fb4ed26c5ff2317db3e589c632ea4d8fd8d0f1af
Dragonir44 <dragonir44@gmail.com>
2023-12-01 21:13:21 +0100

    Refactor vampire spawn rates in vampire forest

---
+++ client/kubejs/startup_scripts/spawnVampire.js
@@ -0,0 +1,26 @@
+// increase spawn rate of vampire baron in the vampire forest
+WorldgenEvents.add(event => {
+    event.addSpawn(prop => {
+        prop.category = 'monster'
+        prop.entity = 'vampirism:vampire_baron'
+        prop.biomes = ['vampirism:vampire_forest']
+        prop.maxCount = 10
+        prop.minCount = 1
+        prop.weight = 100
+    })
+
+    event.addSpawn(prop => {
+        prop.category = 'monster'
+        prop.entity = 'vampirism:vampire'
+        prop.maxCount = 10
+        prop.minCount = 1
+        prop.weight = 80
+    })
+    event.addSpawn(prop => {
+        prop.category = 'monster'
+        prop.entity = 'vampirism:advanced_vampire'
+        prop.maxCount = 10
+        prop.minCount = 1
+        prop.weight = 80
+    })
+})

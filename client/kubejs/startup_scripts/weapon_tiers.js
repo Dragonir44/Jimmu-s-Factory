27945d86441040135e94c851b39e90ef34fea7b0
Dragonir44 <dragonir44@gmail.com>
2023-12-03 15:03:15 +0100

    Update weapon tiers in startup_scripts

--- client/kubejs/startup_scripts/weapon_tiers.js
+++ client/kubejs/startup_scripts/weapon_tiers.js
@@ -1,10 +1,18 @@
+const baseAttack = 4.0
+
 ItemEvents.toolTierRegistry(e => {
     e.add('elysium_tier', tier => {
         tier.uses = 100000
-        tier.speed = 6.0
-        tier.attackDamageBonus = 46.0
+        tier.attackDamageBonus = 50.0 - baseAttack
         tier.level = 4
         tier.enchantmentValue = 14
         tier.repairIngredient = '#forge:ingots/netherite'
+    })
+
+    e.add('cosmic_tier', tier => {
+        tier.uses = -1
+        tier.attackDamageBonus = 1000000.0 - baseAttack
+        tier.level = 10
+        tier.enchantmentValue = 0
     })
 })

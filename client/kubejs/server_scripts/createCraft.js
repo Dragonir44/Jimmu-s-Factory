f6bd10fed5802cea9fe1ed3682cedc4b38fc16f1
Dragonir44 <dragonir44@gmail.com>
2023-12-15 10:29:37 +0100

    Update createCraft.js with new recipes

--- client/kubejs/server_scripts/createCraft.js
+++ client/kubejs/server_scripts/createCraft.js
@@ -3,13 +3,16 @@ ServerEvents.recipes(e => {
 		Item.of('minecraft:diamond').withChance(130.0),
         Item.of('minecraft:charcoal').withChance(50.0),
 	], 'minecraft:coal_block', [
-		// e.recipes.createDeploying('kubejs:diamonded_coal', ['kubejs:diamonded_coal', '64x minecraft:coal']),
         e.recipes.createPressing('kubejs:diamonded_coal', 'kubejs:diamonded_coal'),
-	]).transitionalItem('kubejs:diamonded_coal').loops(64) // set the transitional item and the number of loops
+	]).transitionalItem('kubejs:diamonded_coal').loops(64)
 
 	e.recipes.createEmptying([
 		'minecraft:bucket', 
 		Fluid.of('createchromaticreturn:refined_mixture', 1000)
 	], 'createchromaticreturn:refined_mixture_bucket')
-	console.info(JSON.stringify(Fluid.of('createchromaticreturn:refined_mixture')))
+
+	e.recipes.createEmptying([
+		'minecraft:bucket', 
+		Fluid.of('createchromaticreturn:shadow_essence', 1000)
+	], 'createchromaticreturn:shadow_essence_bucket')
 })

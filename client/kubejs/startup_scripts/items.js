f2668fa9f765e1bad023be4f162ce3005fdd6232
Dragonir44 <dragonir44@gmail.com>
2023-12-15 21:43:57 +0100

    Add burn time to charcoal block

--- client/kubejs/startup_scripts/items.js
+++ client/kubejs/startup_scripts/items.js
@@ -4,3 +4,9 @@ StartupEvents.registry('item', e => {
         .unstackable()
         .texture('kubejs:item/diamonded_coal')
 })
+
+ItemEvents.modification(e => {
+    e.modify('kubejs:charcoal_block', item => {
+        item.burnTime = 1600 * 10
+    })
+})

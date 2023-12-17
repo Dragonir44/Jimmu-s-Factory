437280dec9c2251dcb5837d1a79d14c425864548
Dragonir44 <dragonir44@gmail.com>
2023-12-17 14:27:48 +0100

    Update swing physics for hat stand and enable placing books on the ground

---
+++ server/kubejs/startup_scripts/brewing.js
@@ -0,0 +1,9 @@
+MoreJSEvents.registerPotionBrewing(e => {
+    e.addCustomBrewing(
+        "minecraft:torchflower", 
+        Ingredient.customNBT("minecraft:potion", (nbt) => {
+            return nbt.contains("Potion") && nbt.Potion == "minecraft:water";
+        }), 
+        Item.of("kubejs:bang")
+    )
+})

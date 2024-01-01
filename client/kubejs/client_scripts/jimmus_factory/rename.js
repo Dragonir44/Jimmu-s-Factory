bcfa86f96658738e92e6411ece5e98b2c0e2aa23
Dragonir44 <dragonir44@gmail.com>
2024-01-01 10:37:11 +0100

    Add new fluid type translations

---
+++ client/kubejs/client_scripts/jimmus_factory/rename.js
@@ -0,0 +1,5 @@
+ClientEvents.lang('en_us', e => {
+    e.add('fluid_type.createchromaticreturn.refined_mixture', 'Refined Mixture')
+    e.add('fluid_type.createchromaticreturn.shadow_essence', 'Shadow Essence')
+    e.add('fluid_type.create_things_and_misc.diluted_bonemeal', 'Diluted Bonemeal')
+})

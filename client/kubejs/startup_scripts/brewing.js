1325c8b0f6569bf6c1779e14b69532a874c81b08
Dragonir44 <dragonir44@gmail.com>
2024-01-01 10:39:27 +0100

    Remove unused startup scripts

--- client/kubejs/startup_scripts/brewing.js
+++
@@ -1,9 +0,0 @@
-MoreJSEvents.registerPotionBrewing(e => {
-    e.addCustomBrewing(
-        "minecraft:torchflower", 
-        Ingredient.customNBT("minecraft:potion", (nbt) => {
-            return nbt.contains("Potion") && nbt.Potion == "minecraft:water";
-        }), 
-        Item.of("kubejs:bang")
-    )
-})

1d0229efcdfb99549947c3480b96f9409fc431ba
Dragonir44 <dragonir44@gmail.com>
2024-01-01 10:37:24 +0100

    Remove unused language translations

--- client/kubejs/client_scripts/rename.js
+++
@@ -1,4 +0,0 @@
-ClientEvents.lang('en_us', e => {
-    e.add('fluid_type.createchromaticreturn.refined_mixture', 'Refined Mixture')
-    e.add('fluid_type.createchromaticreturn.shadow_essence', 'Shadow Essence')
-})

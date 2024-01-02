dbbc7da85f3d90143338086e52304324a95be276
Dragonir44 <dragonir44@gmail.com>
2024-01-02 09:59:59 +0100

    Remove unused code for firing fireworks

--- client/kubejs/client_scripts/balancedflight/items.js
+++
@@ -1,8 +0,0 @@
-const FireworkRocketEntity = Java.loadClass('net.minecraft.world.entity.projectile.FireworkRocketEntity')
-const ItemStack = Java.loadClass('net.minecraft.world.item.ItemStack')
-
-NetworkEvents.dataReceived('useFireworks', event => {
-    const {player} = event
-    console.log(new FireworkRocketEntity(player.getLevel(), player.x, player.y, player.z, Item.of('minecraft:firework_rocket')))
-    player.level.addFreshEntity(new FireworkRocketEntity(player.getLevel(), player.x, player.y, player.z, Item.of('minecraft:firework_rocket')))
-})

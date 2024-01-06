3c532badb56bcbe4965dc4cd91b9339ceff0ebe2
Dragonir44 <dragonir44@gmail.com>
2024-01-06 19:36:59 +0100

    Add new mechanical crafting recipes for creative items

--- client/kubejs/server_scripts/jimmus_factory/createCraft.js
+++ client/kubejs/server_scripts/jimmus_factory/createCraft.js
@@ -25,4 +25,48 @@ ServerEvents.recipes(e => {
 		'minecraft:bucket',
 		Fluid.of('create_things_and_misc:diluted_bonemeal', 1000),
 	])
+
+	e.recipes.createMechanicalCrafting('create:creative_crate', [
+		"MNCNM",
+		"NNRNN",
+		"NRWRN",
+		"NNRNN",
+		"MNCNM"
+	],{
+		N: 'minecraft:netherite_block',
+		M: 'createchromaticreturn:multiplite_ingot',
+		R: 'createchromaticreturn:refined_mechanism',
+		W: 'cyclic:crate',
+		C: 'createchromaticreturn:creative_flour'
+	})
+
+	e.recipes.createMechanicalCrafting('create:creative_fluid_tank', [
+		"MNFNM",
+		"NNRNN",
+		"NRTRN",
+		"NNRNN",
+		"MNFNM"
+	],{
+		N: 'minecraft:netherite_block',
+		M: 'createchromaticreturn:multiplite_ingot',
+		R: 'createchromaticreturn:refined_mechanism',
+		T: 'create:fluid_tank',
+		F: 'create_sa:creative_filling_tank'
+	})
+
+	e.recipes.createMechanicalCrafting('create:handheld_worldshaper', [
+		"  NNNNNN",
+		"  WFFFFF",
+		"UUGNNNNN",
+		"UUCFFFFF",
+		"  RR    "
+	],{
+		N: "minecraft:netherite_block",
+		F: 'createchromaticreturn:creative_flour',
+		W: 'createcasing:creative_cogwheel',
+		G: 'createaddition:creative_energy',
+		C: 'create:creative_crate',
+		R: 'createchromaticreturn:refined_mechanism',
+		U: 'extendedcrafting:ultimate_singularity'
+	})
 })

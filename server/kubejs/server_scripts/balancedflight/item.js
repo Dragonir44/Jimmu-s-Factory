bad408166300860aeaf09e12e06e3033ecd98268
Dragonir44 <dragonir44@gmail.com>
2024-01-10 19:30:14 +0100

    Update configuration and language files, add new items and slots

--- server/kubejs/server_scripts/balancedflight/item.js
+++ server/kubejs/server_scripts/balancedflight/item.js
@@ -1,20 +1,16 @@
 PlayerEvents.tick(event => {
     const {player, player: {inventory}} = event
 
-    player.hasRing = inventory.contains('kubejs:ascended_flight_ring')
-
-    if (inventory.contains('kubejs:ascended_flight_ring') && !player.hasRing) {
-        if (player.gameMode != 'creative' && !player.abilities.mayfly) {
+    if (inventory.contains('kubejs:ascended_flight_ring')) {
+        if (!player.gameMode.isCreative()) {
             player.abilities.mayfly = true
-            player.hasRing = true
         }
     }
-
-    if (!inventory.contains('kubejs:ascended_flight_ring') && player.hasRing) {
-        if (player.gameMode != 'creative' && player.abilities.mayfly) {
+    
+    if (!inventory.contains('kubejs:ascended_flight_ring') && !player.vampAtts.vampSpecial.bat) {
+        if (!player.gameMode.isCreative()) {
             player.abilities.mayfly = false
             player.abilities.flying = false
-            player.hasRing = false
         }
     }
     player.onUpdateAbilities()
--- server/kubejs/startup_scripts/balancedflight/item.js
+++ server/kubejs/startup_scripts/balancedflight/item.js
@@ -3,6 +3,6 @@ StartupEvents.registry('item', e => {
         .displayName('Ascended Flight Ring')
         .unstackable()
         .tooltip(Text.gold('An incridibly dense golden ring. Despite its weight, it allows you to fly anywhere (Angel Ring)'))
-        .tooltip('Allows both creative and enabled Elytra flight')
+        .tooltip('Allows creative flight')
         .texture('balancedflight:item/ascended_flight_ring')
 })

6a1d704a2e9f14cd8ccc45dcc95e6ca8a58e2e60
Dragonir44 <dragonir44@gmail.com>
2024-01-28 12:25:10 +0100

    Hide and remove creative items and recipes in JEI

--- client/kubejs/client_scripts/jimmus_factory/jei.js
+++ client/kubejs/client_scripts/jimmus_factory/jei.js
@@ -1,3 +1,35 @@
+const hiddenItems = [
+    "cyclic:battery_infinite",
+    "bigreactors:reinforced_reactorcreativewatergenerator",
+    "bigreactors:basic_turbinecreativesteamgenerator",
+    "bigreactors:reinforced_turbinecreativesteamgenerator",
+    "storagedrawers:creative_storage_upgrade",
+    "storagedrawers:creative_vending_upgrade",
+    "born_in_chaos_v1:supreme_measure",
+    "create_sa:creative_filling_tank",
+    "ars_nouveau:creative_spell_book",
+    "ars_nouveau:creative_source_jar",
+    "botania:creative_pool",
+    "botania:infrangible_platform",
+    "botania:corporea_spark_creative",
+    "electrodynamics:creativepowersource",
+    "electrodynamics:creativefluidsource",
+    "mekanism:creative_bin",
+    "mekanism:creative_fluid_tank",
+    "mekanism:creative_energy_cube",
+    "mekanism:creative_chemical_tank",
+    "mysticalagriculture:creative_soulium_dagger",
+    "mysticalagradditions:creative_essence"
+]
+
 JEIEvents.hideItems(event => {
-        
+    for(const hiddenItem of hiddenItems) {
+        event.hide(hiddenItem)
+    }
+})
+
+JEIEvents.removeRecipes(event => {
+    for(const hiddenItem of hiddenItems) {
+        event.remove(hiddenItem)
+    }
 })

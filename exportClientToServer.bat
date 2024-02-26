6a668b8448e8120b4d294eaef7f0de1b381b70cc
Dragonir44 <dragonir44@gmail.com>
2024-02-26 17:00:04 +0100

    Mise à jour de l'exportation des fichiers clients vers le serveur

--- exportClientToServer.bat
+++ exportClientToServer.bat
@@ -4,26 +4,45 @@ set "clientModsPath=client/mods"
 set "serverModsPath=server/mods"
 set "excludeFiles=exclude.txt"
 
+set "clientConfigPath=client/config"
+set "serverConfigPath=server/config"
+
+set "clientKubePath=client/kubejs"
+set "serverKubePath=server/kubejs"
+
 REM Create a temporary directory
-set "tempDir=%TEMP%\exportClientToServer_%RANDOM%"
-mkdir "%tempDir%"
+set "tempDirMods=%TEMP%\exportClientToServerMods_%RANDOM%"
+set "tempDirConfig=%TEMP%\exportClientToServerConfig_%RANDOM%"
+set "tempDirKube=%TEMP%\exportClientToServerKube_%RANDOM%"
+mkdir "%tempDirMods%"
+mkdir "%tempDirConfig%"
+mkdir "%tempDirKube%"
 
 REM Copy all files from client/mods to the temporary directory
-xcopy /s /e /i "%clientModsPath%" "%tempDir%"
+xcopy /s /e /i "%clientModsPath%" "%tempDirMods%"
+xcopy /s /e /i "%clientConfigPath%" "%tempDirConfig%"
+xcopy /s /e /i "%clientKubePath%" "%tempDirKube%"
+
 
 REM Remove the excluded files from the temporary directory
 for /f "usebackq delims=" %%f in ("%excludeFiles%") do (
     echo Removing %%f
-    del /q "%tempDir%\%%f" >nul 2>&1
+    del /q "%tempDirMods%\%%f" >nul 2>&1
 )
 
 REM Remove the existing files in server/mods
 rmdir /s /q "%serverModsPath%"
+rmdir /s /q "%serverConfigPath%"
+rmdir /s /q "%serverKubePath%"
 
 REM Move the files from the temporary directory to server/mods
-move "%tempDir%" "%serverModsPath%"
+move "%tempDirMods%" "%serverModsPath%"
+move "%tempDirConfig%" "%serverConfigPath%"
+move "%tempDirKube%" "%serverKubePath%"
 
 REM Clean up the temporary directory
-rmdir /s /q "%tempDir%"
+rmdir /s /q "%tempDirMods%"
+rmdir /s /q "%tempDirConfig%"
+rmdir /s /q "%tempDirKube%"
 
 echo Files copied successfully.

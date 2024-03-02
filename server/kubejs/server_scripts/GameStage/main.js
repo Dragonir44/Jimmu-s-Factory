0e9ede88bd5492b602cf26cd3fc7c3f18ea4a027
Dragonir44 <dragonir44@gmail.com>
2024-03-02 15:59:42 +0100

    Mise à jour des recettes de craft pour les mods create, ars_nouveau, mna, botania, mekanism, bigreactors et mysticalagriculture

--- server/kubejs/server_scripts/GameStage/main.js
+++ server/kubejs/server_scripts/GameStage/main.js
@@ -1,104 +1,57 @@
 ServerEvents.recipes((event) => {
-    event.forEachRecipe({mod: "create"}, r => {
+    event.forEachRecipe({mod: "create", type: "crafting_shaped" || "crafting_shapeless"}, r => {
         let recipes = r.json
 
-        if (r.getType() == "minecraft:crafting_shaped" || r.getType() == "minecraft:crafting_shapeless") {
-            if (r.getType() == "minecraft:crafting_shaped") {
-                recipes.addProperty("type", "kubejs:shaped")
-            }
-            if (r.getType() == "minecraft:crafting_shapeless") {
-                recipes.addProperty("type", "kubejs:shapeless")
-            }
-            
-            event.custom(recipes).id(r.getId()).stage("create")
-        }
+        recipes.addProperty("type", r.getType() == "minecraft:crafting_shaped" ? "kubejs:shaped" : r.getType() == "minecraft:crafting_shapeless" ? "kubejs:shapeless" : null)
+        
+        event.custom(recipes).id(r.getId()).stage("create")
     })
 
     event.forEachRecipe({mod: 'ars_nouveau'}, r => {
         let recipes = r.json
 
-        if (r.getType() == "minecraft:crafting_shaped" || r.getType() == "minecraft:crafting_shapeless") {
-            if (r.getType() == "minecraft:crafting_shaped") {
-                recipes.addProperty("type", "kubejs:shaped")
-            }
-            if (r.getType() == "minecraft:crafting_shapeless") {
-                recipes.addProperty("type", "kubejs:shapeless")
-            }
-            
-            event.custom(recipes).id(r.getId()).stage("ars_nouveau")
-        }
+        recipes.addProperty("type", r.getType() == "minecraft:crafting_shaped" ? "kubejs:shaped" : r.getType() == "minecraft:crafting_shapeless" ? "kubejs:shapeless" : null)
+
+        event.custom(recipes).id(r.getId()).stage("ars_nouveau")
     })
 
     event.forEachRecipe({mod: 'mna'}, r => {
         let recipes = r.json
         
-        if (r.getType() == "minecraft:crafting_shaped" || r.getType() == "minecraft:crafting_shapeless") {
-            if (r.getType() == "minecraft:crafting_shaped") {
-                recipes.addProperty("type", "kubejs:shaped")
-            }
-            if (r.getType() == "minecraft:crafting_shapeless") {
-                recipes.addProperty("type", "kubejs:shapeless")
-            }
-            event.custom(recipes).id(r.getId()).stage("mna")
-        }
+        recipes.addProperty("type", r.getType() == "minecraft:crafting_shaped" ? "kubejs:shaped" : r.getType() == "minecraft:crafting_shapeless" ? "kubejs:shapeless" : null)
+
+        event.custom(recipes).id(r.getId()).stage("mna")
     })
 
     event.forEachRecipe({mod: 'botania'}, r => {
         let recipes = r.json
 
-        if (r.getType() == "minecraft:crafting_shaped" || r.getType() == "minecraft:crafting_shapeless") {
-            if (r.getType() == "minecraft:crafting_shaped") {
-                recipes.addProperty("type", "kubejs:shaped")
-            }
-            if (r.getType() == "minecraft:crafting_shapeless") {
-                recipes.addProperty("type", "kubejs:shapeless")
-            }
-            event.custom(recipes).id(r.getId()).stage("botania")
-        }
+        recipes.addProperty("type", r.getType() == "minecraft:crafting_shaped" ? "kubejs:shaped" : r.getType() == "minecraft:crafting_shapeless" ? "kubejs:shapeless" : null)
+
+        event.custom(recipes).id(r.getId()).stage("botania")
     })
 
     event.forEachRecipe({mod: 'mekanism'}, r => {
         let recipes = r.json
 
-        if (r.getType() == "minecraft:crafting_shaped" || r.getType() == "minecraft:crafting_shapeless") {
-            if (r.getType() == "minecraft:crafting_shaped") {
-                recipes.addProperty("type", "kubejs:shaped")
-            }
-            if (r.getType() == "minecraft:crafting_shapeless") {
-                recipes.addProperty("type", "kubejs:shapeless")
-            }
-            
-            event.custom(recipes).id(r.getId()).stage("mekanism")
-        }
+        recipes.addProperty("type", r.getType() == "minecraft:crafting_shaped" ? "kubejs:shaped" : r.getType() == "minecraft:crafting_shapeless" ? "kubejs:shapeless" : null)
+
+        event.custom(recipes).id(r.getId()).stage("mekanism")
     })
 
     event.forEachRecipe({mod: 'bigreactors'}, r => {
         let recipes = r.json
 
-        if (r.getType() == "minecraft:crafting_shaped" || r.getType() == "minecraft:crafting_shapeless") {
-            if (r.getType() == "minecraft:crafting_shaped") {
-                recipes.addProperty("type", "kubejs:shaped")
-            }
-            if (r.getType() == "minecraft:crafting_shapeless") {
-                recipes.addProperty("type", "kubejs:shapeless")
-            }
-            
-            event.custom(recipes).id(r.getId()).stage("bigreactors")
-        }
+        recipes.addProperty("type", r.getType() == "minecraft:crafting_shaped" ? "kubejs:shaped" : r.getType() == "minecraft:crafting_shapeless" ? "kubejs:shapeless" : null)
+
+        event.custom(recipes).id(r.getId()).stage("bigreactors")
     })
 
     event.forEachRecipe({mod: 'mysticalagriculture'}, r => {
         let recipes = r.json
 
-        if (r.getType() == "minecraft:crafting_shaped" || r.getType() == "minecraft:crafting_shapeless") {
-            if (r.getType() == "minecraft:crafting_shaped") {
-                recipes.addProperty("type", "kubejs:shaped")
-            }
-            if (r.getType() == "minecraft:crafting_shapeless") {
-                recipes.addProperty("type", "kubejs:shapeless")
-            }
-            
-            event.custom(recipes).id(r.getId()).stage("mysticalagriculture")
-        }
+        recipes.addProperty("type", r.getType() == "minecraft:crafting_shaped" ? "kubejs:shaped" : r.getType() == "minecraft:crafting_shapeless" ? "kubejs:shapeless" : null)
+
+        event.custom(recipes).id(r.getId()).stage("mysticalagriculture")
     })
 })

2d4594b3841f520d46e23f5cf7908c40622cb1c4
Dragonir44 <dragonir44@gmail.com>
2024-03-05 22:43:30 +0100

    Mise à jour de la gestion des étapes de recette

--- client/kubejs/server_scripts/GameStage/refreshMatrix.js
+++ client/kubejs/server_scripts/GameStage/refreshMatrix.js
@@ -1,3 +1,21 @@
 PlayerEvents.loggedIn(event => {
-    event.server.runCommandSilent('/sync_recipe_stages')
+    var player_stages = event.player.getStages().getAll()
+    var all_stages = Java.loadClass('com.blamejared.recipestages.RecipeStages').PACKAGE_STAGES;
+    for (var mod of modsWithCraftingTable) {
+        all_stages.putIfAbsent(mod, Java.loadClass("java.util.HashSet")())
+        var mod_stages = all_stages[mod]
+        mod_stages.clear()
+        mod_stages.addAll(player_stages)
+    }
+})
+
+GameStageEvents.stageAdded(event => {
+    var player_stages = event.player.getStages().getAll()
+    var all_stages = Java.loadClass('com.blamejared.recipestages.RecipeStages').PACKAGE_STAGES;
+    for (var mod of modsWithCraftingTable) {
+        all_stages.putIfAbsent(mod, Java.loadClass("java.util.HashSet")())
+        var mod_stages = all_stages[mod]
+        mod_stages.clear()
+        mod_stages.addAll(player_stages)
+    }
 })

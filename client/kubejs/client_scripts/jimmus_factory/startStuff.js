75dbc611d6088cad924629ed3b1362581d803cd6
Dragonir44 <dragonir44@gmail.com>
2024-03-24 18:13:18 +0100

    Ajout du fichier startStuff.js avec les modifications nécessaires

---
+++ client/kubejs/client_scripts/jimmus_factory/startStuff.js
@@ -0,0 +1,1 @@
+const eccentrictome = Item.of('eccentrictome:tome', '{"eccentrictome:mods":{alexsmobs:{0:{Count:1b,id:"alexsmobs:animal_dictionary"}},ars_nouveau:{0:{Count:1b,id:"ars_nouveau:worn_notebook"}}}}')

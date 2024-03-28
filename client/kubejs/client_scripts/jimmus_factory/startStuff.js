bd4ed49866a3a8f6c1ad5ee17a20e4239b5b6900
Dragonir44 <dragonir44@gmail.com>
2024-03-28 19:47:06 +0100

    Ajouter eccentrictome à l'inventaire du joueur lorsqu'il rejoint le monde

--- client/kubejs/client_scripts/jimmus_factory/startStuff.js
+++ client/kubejs/client_scripts/jimmus_factory/startStuff.js
@@ -1,1 +1,3 @@
 const eccentrictome = Item.of('eccentrictome:tome', '{"eccentrictome:mods":{alexsmobs:{0:{Count:1b,id:"alexsmobs:animal_dictionary"}},ars_nouveau:{0:{Count:1b,id:"ars_nouveau:worn_notebook"}}}}')
+
+// add the eccentrictome to the player's inventory when they first join the world

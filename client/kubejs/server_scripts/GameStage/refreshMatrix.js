7dbb23db0998e07f2ac9bb71f584df7650fb563b
Dragonir44 <dragonir44@gmail.com>
2024-05-01 15:48:24 +0200

    Mise à jour du script refreshMatrix.js pour commenter le code obsolète

--- client/kubejs/server_scripts/GameStage/refreshMatrix.js
+++ client/kubejs/server_scripts/GameStage/refreshMatrix.js
@@ -1,13 +1,13 @@
-PlayerEvents.loggedIn(event => {
-    var player_stages = event.player.getStages().getAll()
-    var all_stages = Java.loadClass('com.blamejared.recipestages.RecipeStages').PACKAGE_STAGES;
-    for (var mod of modsWithCraftingTable) {
-        all_stages.putIfAbsent(mod, Java.loadClass("java.util.HashSet")())
-        var mod_stages = all_stages[mod]
-        mod_stages.clear()
-        mod_stages.addAll(player_stages)
-    }
-})
+// PlayerEvents.loggedIn(event => {
+//     var player_stages = event.player.getStages().getAll()
+//     var all_stages = Java.loadClass('com.blamejared.recipestages.RecipeStages').PACKAGE_STAGES;
+//     for (var mod of modsWithCraftingTable) {
+//         all_stages.putIfAbsent(mod, Java.loadClass("java.util.HashSet")())
+//         var mod_stages = all_stages[mod]
+//         mod_stages.clear()
+//         mod_stages.addAll(player_stages)
+//     }
+// })
 
 GameStageEvents.stageAdded(event => {
     var player_stages = event.player.getStages().getAll()
